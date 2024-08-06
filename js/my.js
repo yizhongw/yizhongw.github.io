53262c6933119c5d163cc48c9b88e6d7997f27a9
yizhongw <eastonwyz@gmail.com>
2024-08-06 11:23:13 -0700

    Update formats and styles.

---
+++ js/my.js
@@ -0,0 +1,8 @@
+function toggle_old_news() {
+    var x = document.getElementById("old-news");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
